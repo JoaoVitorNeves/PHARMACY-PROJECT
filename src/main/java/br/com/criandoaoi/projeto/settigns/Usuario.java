ad8f4f823f332b50f0441e881934a6aa0daeb1a3
JOAOVITORNEVES <jvitornevessilva@gmail.com>
2022-06-04 19:11:03 -0300

    First Commit,project starting

---
+++ src/main/java/br/com/criandoaoi/projeto/settigns/Usuario.java
@@ -0,0 +1,75 @@
+package br.com.criandoaoi.projeto.settigns;
+import br.com.criandoaoi.projeto.controller.*;
+import br.com.criandoaoi.projeto.DAO.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+	private String username;
+
+	private String email;
+	
+	private String senha;
+
+	private String telefone;
+	
+	private String nome_completo;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsernome() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNome_completo() {
+		return nome_completo;
+	}
+
+	public void setNome_completo(String nome_completo) {
+		this.nome_completo = nome_completo;
+	}
+	
+	
+}
